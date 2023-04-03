ce2154dd7c068340c184bcd2d6df8e2f24531b62
isabelmariamelo <immelo@syr.edu>
2023-04-02 23:29:21 -0400

    user input prompted for free busy block api call

--- py/freeBusyAPI.py
+++ py/freeBusyAPI.py
@@ -11,26 +11,27 @@ start_time = datetime.now(timezone.utc)
 end_time = start_time + timedelta(days=7)
 time_min = start_time.isoformat()
 time_max = end_time.isoformat()
+while(1):
+    userId = input("please enter your user id: ")
+    freebusy_query = {
+        'timeMin': time_min,
+        'timeMax': time_max,
+        'timeZone': 'UTC',
+        'items': [{'id': userId}]
+    }
 
-freebusy_query = {
-    'timeMin': time_min,
-    'timeMax': time_max,
-    'timeZone': 'UTC',
-    'items': [{'id': 'primary'}]
-}
+    try:
+        freebusy_result = service.freebusy().query(body=freebusy_query).execute()
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        freebusy_result = None
 
-try:
-    freebusy_result = service.freebusy().query(body=freebusy_query).execute()
-except HttpError as error:
-    print(f'An error occurred: {error}')
-    freebusy_result = None
-
-if freebusy_result is None:
-    print('No free/busy information available.')
-else:
-    for cal_id, cal_data in freebusy_result['calendars'].items():
-        print(f'Free/busy information for calendar {cal_id}:')
-        for busy in cal_data['busy']:
-            start_time = busy['start']
-            end_time = busy['end']
-            print(f'{start_time} to {end_time}')
+    if freebusy_result is None:
+        print('No free/busy information available.')
+    else:
+        for cal_id, cal_data in freebusy_result['calendars'].items():
+            print(f'Free/busy information for calendar {cal_id}:')
+            for busy in cal_data['busy']:
+                start_time = busy['start']
+                end_time = busy['end']
+                print(f'{start_time} to {end_time}')
